b0fcae6e7d25213d55be52fe1cf3cdcf8a9e1ba5
Bryan31542 <00063820@uca.edu.sv>
2022-08-06 16:23:08 -0600

    feat: setting desk screen

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,33 @@
+// HTMLs Refs
+const lblDesk = document.querySelector("h1");
+const btnAttend = document.querySelector("button");
 
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+if (!searchParams.has("escritorio")) {
+  window.location = "index.html";
+  throw new Error("El escritorio es obligatorio");
+}
+
+const escritorio = searchParams.get("escritorio");
+lblDesk.innerText = escritorio;
+
+const socket = io();
+
+socket.on("connect", () => {
+  btnCreate.disabled = false;
+});
+
+socket.on("disconnect", () => {
+  btnCreate.disabled = true;
+});
+
+socket.on("last-ticket", (last) => {
+//   lblNewTicket.innerText = "Ticket " + last;
+});
+
+btnAttend.addEventListener("click", () => {
+//   socket.emit("next-ticket", null, (ticket) => {
+//     lblNewTicket.innerText = ticket;
+//   });
+});
