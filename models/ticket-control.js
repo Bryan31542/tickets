9724e656b9d579b2719236f3c8cbdbd6443cbe31
Bryan31542 <00063820@uca.edu.sv>
2022-08-06 15:39:29 -0600

    feat: next and attend ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,6 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 
+class Ticket {
+  constructor(number, desk) {
+    this.number = "";
+    this.desk = "";
+  }
+}
+
 class TicketControl {
   constructor() {
     this.last = 0;
@@ -36,6 +43,38 @@ class TicketControl {
   saveDB() {
     const dbPath = path.join(__dirname, "../db/data.json");
     fs.writeFileSync(dbPath, JSON.stringify(this.toJSON));
+  }
+
+  nextTicket() {
+    this.last += 1;
+    const ticket = new Ticket(this.last, null);
+    this.tickets.push(ticket);
+
+    this.saveDB();
+
+    return `Ticket ${this.number}`;
+  }
+
+  attendTicket(desk) {
+    // No tickets
+    if (this.tickets.length === 0) {
+      return null;
+    }
+
+    const ticket = this.tickets.shift();
+    ticket.desk = desk;
+
+    // adding at the begining of the array
+    this.lastFour.unshift(ticket);
+
+    // if the array is greater than 4, remove the last element
+    if (this.lastFour.length > 4) {
+      this.lastFour.splice(-1, 1);
+    }
+
+    this.saveDB();
+
+    return ticket;
   }
 }
 
